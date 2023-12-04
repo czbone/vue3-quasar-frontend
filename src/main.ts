db853faf5b4d1a6fa5359f4c13a5cc69a4f0b1a1
naoki hirata <naoki@magic3.org>
2023-12-04 12:13:19 +0900

    初期登録。

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/ja' // 日本語対応
+
+// CSS読み込み
+import '@quasar/extras/material-icons/material-icons.css' // フォントアイコン
+import 'quasar/src/css/index.sass' // Quasar CSS
+import './styles/app.scss' // CSSをカスタマイズ
+
+import App from './App.vue'
+import router from './router'
+
+// アプリケーション作成
+const app = createApp(App)
+
+// 言語、プラグイン追加
+app.use(Quasar, {
+  lang: quasarLang,
+  plugins: {} // プラグイン
+})
+
+// Pinia初期化
+app.use(createPinia())
+
+// ルータ初期化
+app.use(router)
+
+// index.htmlに連結
+app.mount('#app')
---
+++ src/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <QPage class="row justify-center" padding>
+    <div class="contents">
+      <HelloWorld msg="Vite + Vue 3 + Quasar" />
+    </div>
+  </QPage>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+<style scoped>
+.contents {
+  width: 700px;
+}
+</style>
